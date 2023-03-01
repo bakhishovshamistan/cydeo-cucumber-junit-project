b7282c8b1c84536a5ecea96bb0ea93ecee04899c
Shami Bakhishov <114504766+bakhishovshamistan@users.noreply.github.com>
2023-03-01 09:58:41 -0500

    Update CukesRunner.java

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
-        tags = "@librarian",
+        tags = "@",
       //  monochrome = false, // for coloring
         publish = true // generating a report with public link
 
