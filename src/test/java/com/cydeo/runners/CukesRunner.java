7690a6584dde15d07bad61dce7c1b9c09e8b6f7b
bakhishovshamistan <bakhishov.shamistan@gmail.com>
2023-03-01 15:31:02 -0500

    cucumber-junit-project updated 2

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -16,8 +16,8 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
-        dryRun = false,
-        tags = "@webtable",
+        dryRun = true,
+        tags = "@wip",
       //  monochrome = false, // for coloring
         publish = true // generating a report with public link
 
